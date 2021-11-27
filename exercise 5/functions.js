8b1a5a4fa99e57a58fc1e91656f2b7198ce01b50
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-27 23:58:00 +0100

    Add files via upload

---
+++ exercise 5/functions.js
@@ -0,0 +1,26 @@
+// Making Pizza
+const makePizza = function () {
+    console.log("Roll out the Dough");
+    console.log("Add ingredients ");
+    console.log("Put the pizza in the oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
+
+
+// Sushi Making
+const Sushi = function () {
+    console.log("Make some delicious Sushi");
+}
+
+Sushi();
+
+// Order of 3 sushi & 1 pizza
+Sushi();
+Sushi();
+Sushi();
+makePizza();
+
+
